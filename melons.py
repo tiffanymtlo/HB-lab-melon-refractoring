564a2108de5e5806d8fe7b340bd5e42bacbd660a
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-17 17:14:32 -0800

    completed melon refractoring

---
+++ melons.py
@@ -0,0 +1,54 @@
+"""Classes for melon orders."""
+
+
+class AbstractMelonOrder:
+
+    def __init__(self, species, qty, country_code=None):
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        if country_code:
+            self.country_code = country_code
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        # base_price = 5 
+        base_price = 5 * 1.5
+        total = (1 + self.tax) * self.qty * base_price
+
+        if self.order_type == "international" and self.qty < 10:
+          total += 3
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+    order_type = "domestic"
+    tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+    order_type = "international"
+    tax = 0.17
+
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    
+    passed_inspection = False
+
+    def mark_inspection(self, passed):
+
+        self.passed_inspection = passed
